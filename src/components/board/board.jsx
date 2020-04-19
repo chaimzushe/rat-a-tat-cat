996055bb1b3cfd7487c3a8cb384eb018716cd060
Zushe <chaimzushe@gmail.com>
2020-04-19 08:12:40 -0400

    removed sample files and created board component place holder

---
+++ src/components/board/board.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./board.scss";
+export default class Board extends React.Component {
+
+   
+
+    render(){
+        return <div className="board">
+           
+        </div>
+
+    }
+
+}
